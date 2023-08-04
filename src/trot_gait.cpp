0da031e4729f63689262f37abcc2ca89586806c7
Alpaca-zip <zip.lottestr@gmail.com>
2023-08-04 17:33:18 +0900

    fix LICENSE

--- src/trot_gait.cpp
+++ src/trot_gait.cpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2022  Alpaca-zip
+ * Copyright (C) 2021-2023  Alpaca-zip
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
